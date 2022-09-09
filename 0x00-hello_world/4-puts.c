9aa8a9499f4550bc3ea243117b868c45c8c71583
IyanuAyodele <iyanuayodele9@gmail.com>
2022-09-08 18:47:40 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,1 +1,12 @@
-#!/bin/bash
+#include<stdio.h>
+/**
+* main-Entry Point
+*
+* Return always 0 (success)
+*/
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
+
