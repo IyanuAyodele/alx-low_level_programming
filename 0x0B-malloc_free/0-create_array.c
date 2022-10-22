a2eeb80158de5e7ba9c355569039b3b6404abced
IyanuAyodele <iyanuayodele9@gmail.com>
2022-10-22 04:56:46 -0700

    0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -20,7 +20,8 @@ char *create_array(unsigned int size, char c)
 	if (cp == NULL)
 		return (NULL);
 
-	for (i = 0; i < size; i++)		cp[i] = c;
+	for (i = 0; i < size; i++)	
+		cp[i] = c;
 
 	return (cp);
 }
